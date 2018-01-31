f8912629f72ac88e19f0ed593a911013834e3b35
stabudesk <stabudesk@gmail.com>
2018-01-31 22:16:30 +0000

    got rid of depth file options, of which, 2. pstr and qstr. -u -y -a not quite ready yet

--- gffsimp.c
+++ gffsimp.c
@@ -111,8 +111,6 @@ typedef struct /* opt_t, a struct for the options */
 	char *istr; /* first bedgraph file, the target of the filtering by the second */
 	char *fstr; /* the name of the feature bed file, often converted from GFF3 */
 	char *ustr; /* the name of a file with the list of elements to be unified */
-	char *pstr; /* depth file name */
-	char *qstr; /* 2nddepth file name */
 	char *gstr; /* genome file name */
 	char *rstr; /* repeatmasker gtf/gff2 file */
 	char *ystr; /* the gf22_t type */
@@ -201,14 +199,6 @@ typedef struct /* words_t: file with only single words per line */
 	size_t nsz; /* size of the name r ID field */
 } words_t; /* bedgraph row type */
 
-typedef struct /* dpf_t : depth file type ... just chr name, pos and read quant: so, single base-locus positions, yes? */
-{
-	char *n;
-	size_t nsz; /* size of the name r ID field */
-	long p; /* position */
-	int d; /* depth reading */
-} dpf_t;
-
 typedef struct /* gf_t : genome file type ... just chr name, pos and read quant */
 {
 	char *n;
@@ -231,7 +221,7 @@ int catchopts(opt_t *opts, int oargc, char **oargv)
 	int c;
 	opterr = 0;
 
-	while ((c = getopt (oargc, oargv, "dsni:f:u:p:g:r:q:y:a:h:z:b:")) != -1)
+	while ((c = getopt (oargc, oargv, "dsni:f:u:g:r:y:a:h:z:b:")) != -1)
 		switch (c) {
 			case 'd':
 				opts->dflg = 1;
@@ -250,12 +240,6 @@ int catchopts(opt_t *opts, int oargc, char **oargv)
 				break;
 			case 'u': /* unify certain bed2 elements into one file */
 				opts->ustr = optarg;
-				break;
-			case 'p': /* depth file */
-				opts->pstr = optarg;
-				break;
-			case 'q': /* 2nd depth file */
-				opts->qstr = optarg;
 				break;
 			case 'g': /* genome file */
 				opts->gstr = optarg;
@@ -1614,99 +1598,6 @@ gf23_t *processgf23(char *fname, int *m, int *n) /* this is dummy nmae .. it's f
 	return gf23;
 }
 
-dpf_t *processdpf(char *fname, int *m, int *n) /*fourth column is string, other columns to be ignored */
-{
-	/* In order to make no assumptions, the file is treated as lines containing the same amount of words each,
-	 * except for lines starting with #, which are ignored (i.e. comments). These words are checked to make sure they contain only floating number-type
-	 * characters [0123456789+-.] only, one string variable is continually written over and copied into a growing floating point array each time */
-
-	/* declarations */
-	FILE *fp=fopen(fname,"r");
-	int i;
-	size_t couc /*count chars per line */, couw=0 /* count words */, oldcouw = 0;
-	int c;
-	boole inword=0;
-	wseq_t *wa=create_wseq_t(GBUF);
-	size_t bwbuf=WBUF;
-	char *bufword=calloc(bwbuf, sizeof(char)); /* this is the string we'll keep overwriting. */
-
-	dpf_t *dpf=malloc(GBUF*sizeof(dpf_t));
-
-	while( (c=fgetc(fp)) != EOF) { /* grab a char */
-		if( (c== '\n') | (c == ' ') | (c == '\t') | (c=='#')) { /* word closing events */
-			if( inword==1) { /* first word closing event */
-				wa->wln[couw]=couc;
-				bufword[couc++]='\0';
-				bufword = realloc(bufword, couc*sizeof(char)); /* normalize */
-				/* for the struct, we want to know if it's the first word in a line, like so: */
-				if(couw==oldcouw) {
-					dpf[wa->numl].n=malloc(couc*sizeof(char));
-					dpf[wa->numl].nsz=couc;
-					strcpy(dpf[wa->numl].n, bufword);
-				} else if((couw-oldcouw)==1) /* it's not the first word, and it's 1st and second col */
-					dpf[wa->numl].p=atol(bufword)-1L; /* note we convert to o indexed */
-				else if((couw-oldcouw)==2) /* it's not the first word, and it's 1st and second col */
-					dpf[wa->numl].d=atoi(bufword);
-				couc=0;
-				couw++;
-			}
-			if(c=='#') { /* comment case */
-				while( (c=fgetc(fp)) != '\n') ;
-				continue;
-			} else if(c=='\n') { /* end of a line */
-				if(wa->numl == wa->lbuf-1) { /* enought space in our current array? */
-					wa->lbuf += WBUF;
-					wa->wpla=realloc(wa->wpla, wa->lbuf*sizeof(size_t));
-					dpf=realloc(dpf, wa->lbuf*sizeof(bgr_t));
-					memset(wa->wpla+(wa->lbuf-WBUF), 0, WBUF*sizeof(size_t));
-				}
-				wa->wpla[wa->numl] = couw-oldcouw; /* number of words in current line */
-				oldcouw=couw; /* restart words per line count */
-				wa->numl++; /* brand new line coming up */
-			}
-			inword=0;
-		} else if(inword==0) { /* deal with first character of new word, + and - also allowed */
-			if(couw == wa->wsbuf-1) {
-				wa->wsbuf += GBUF;
-				wa->wln=realloc(wa->wln, wa->wsbuf*sizeof(size_t));
-				dpf=realloc(dpf, wa->wsbuf*sizeof(dpf_t));
-				for(i=wa->wsbuf-GBUF;i<wa->wsbuf;++i)
-					wa->wln[i]=0;
-			}
-			couc=0;
-			bwbuf=WBUF;
-			bufword=realloc(bufword, bwbuf*sizeof(char)); /* don't bother with memset, it's not necessary */
-			bufword[couc++]=c; /* no need to check here, it's the first character */
-			inword=1;
-		} else {
-			if(couc == bwbuf-1) { /* the -1 so that we can always add and extra (say 0) when we want */
-				bwbuf += WBUF;
-				bufword = realloc(bufword, bwbuf*sizeof(char));
-			}
-			bufword[couc++]=c;
-		}
-
-	} /* end of big for statement */
-	fclose(fp);
-	free(bufword);
-
-	/* normalization stage */
-	wa->quan=couw;
-	wa->wln = realloc(wa->wln, wa->quan*sizeof(size_t)); /* normalize */
-	dpf = realloc(dpf, wa->quan*sizeof(dpf_t)); /* normalize */
-	wa->wpla= realloc(wa->wpla, wa->numl*sizeof(size_t));
-
-	*m= wa->numl;
-	int k=wa->wpla[0];
-	for(i=1;i<wa->numl;++i)
-		if(k != wa->wpla[i])
-			printf("Warning: Numcols is not uniform at %i words per line on all lines. This file has one with %zu.\n", k, wa->wpla[i]); 
-	*n= k; 
-	free_wseq(wa);
-
-	return dpf;
-}
-
 gf_t *processgf(char *fname, int *m, int *n) /* read in a genome file */
 {
 	/* In order to make no assumptions, the file is treated as lines containing the same amount of words each,
@@ -1871,6 +1762,38 @@ void prtgf22n(char *fname, gf22_t *gf22, int m7, words_t *bedword, int m3) // no
 					break;
 			}
 		}
+	}
+	return;
+}
+
+void prtgf22nfa(i_s *sqisz, gf22_t *gf22, int m7, int sz, words_t *bedword, int m3)
+{
+	int i, j, k;
+	char rangestr[64]={0}; /* generally helpful to say what range is being given */
+	boole foundifeat, foundsq;
+	for(i=0;i<sz;++i) {
+		foundsq=0;
+		for(j=0;j<m7;++j) {
+			if(gf22[i].fc == MRN) {
+				foundifeat=0;
+				foundsq=0;
+				for(k=0;k<m3;++k) {
+					if(!strncmp(bedword[k].n, gf22[i].gbkn, bedword[k].nsz-1) && (!strcmp(sqisz[i].id, gf22[j].n)) ) {
+						foundifeat=1;
+						foundsq=1;
+						sprintf(rangestr, "|range_%li:%li", gf22[j].c[0], gf22[j].c[1]);
+						printf(">%s", sqisz[i].id);
+						printf("%s\n", rangestr);
+						printf("%.*s\n", (int)(gf22[j].c[1]-gf22[j].c[0]), sqisz[i].sq+gf22[j].c[0]);
+						break;
+					}
+				}
+			}
+			if(foundifeat)
+				break;
+		}
+		if(foundsq) // sequence for this feature has been found, move onto next sequence
+			break;
 	}
 	return;
 }
@@ -2600,44 +2523,6 @@ void mgf2bed(char *gfname, char *ffile, gf_t *gf, bgr_t2 *bed2, int m2, int m5) 
 	return;
 }
 
-void mgf2dpf(char *gfname, char *dpffile, gf_t *gf, dpf_t *dpf, int m4, int m5) /* match gf to feature bed file */
-{
-	setlocale(LC_NUMERIC, "");
-	int i, j;
-	long *prdp=calloc(m5, sizeof(long)); /* position read depth */
-	int istarthere=0, catchingi=0;
-	int strmatch;
-	boole caught;
-	printf("Coverage of \"%s\" (genome size file) by \"%s\" (feature bed file):\n", gfname, dpffile); 
-	for(j=0;j<m5;++j) {
-		caught=0;
-		for(i=istarthere;i<m4;++i) {
-			strmatch=strcmp(gf[j].n, dpf[i].n);
-			if( (!strmatch) & (gf[j].z > dpf[i].p)) {
-				prdp[j] += dpf[i].d;
-#ifdef DBG
-				printf("prdp: %'li\n", prdp[j]); 
-#endif
-				catchingi=i;
-				caught=1;
-			} else if (caught) { // will catch first untruth after a series of truths.
-				caught=2;
-				break; // bed1 is ordered so we can forget about trying to match anymore.
-			} else if( (!strmatch) & (gf[j].z <= dpf[i].p)) {
-				printf("There's a problem with the genome size file ... are you sure it's the right one? Bailing out.\n"); 
-				exit(EXIT_FAILURE);
-			}
-		}
-		if(caught==2)
-			istarthere=catchingi+1;
-		printf("%s:\t%'li totreads\t%4.2f avgnumreads\twithin %'li bp\n", gf[j].n, prdp[j], (float)prdp[j]/gf[j].z, gf[j].z);
-		if(istarthere >= m4)
-			break;
-	}
-	free(prdp);
-	return;
-}
-
 void mgf2rmf(char *gfname, char *rmffile, gf_t *gf, rmf_t *rmf, int m6, int m5) /* match gf to feature bed file */
 {
 	setlocale(LC_NUMERIC, "");
@@ -2676,48 +2561,6 @@ void mgf2rmf(char *gfname, char *rmffile, gf_t *gf, rmf_t *rmf, int m6, int m5) 
 			break;
 	}
 	free(acov);
-	return;
-}
-
-void md2bedp(dpf_t *dpf, bgr_t2 *bed2, int m2, int m) /* match up a samtools depth file (-d option) and a feature bed file (-f option) and print */
-{
-	int i, j, min, max;
-	int reghits; /* hits for region: number of lines in bed1 which coincide with a region in bed2 */
-	int cloci; /* as opposed to hit, catch the number of loci */
-	long assoctval=0;
-	int istarthere=0, catchingi=0;
-	boole caught;
-	for(j=0;j<m2;++j) {
-		caught=0;
-		reghits=0;
-		cloci=0;
-		assoctval=0;
-		min=9999999;
-		max=0;
-		for(i=istarthere;i<m;++i) {
-			if( !(strcmp(dpf[i].n, bed2[j].n)) & (dpf[i].p >= bed2[j].c[0]) & (dpf[i].p < bed2[j].c[1]) ) {
-				reghits++;
-				cloci++;
-				assoctval+= dpf[i].d;
-				catchingi=i;
-				if(dpf[i].d<min)
-					min=dpf[i].d;
-				if(dpf[i].d>max)
-					max=dpf[i].d;
-				caught=1;
-			} else if (caught) { // will catch first untruth after a series of truths.
-				caught=2;
-				break; // bed1 is ordered so we can forget about trying to match anymore.
-			}
-		}
-		if(caught==2)
-			istarthere=catchingi+1;
-		// printf("Bed2idx %i / name %s / size %li got %i hits from dpf , being %i loci and accumulated depth val of %lu\n", j, bed2[j].f, bed2[j].c[1]-bed2[j].c[0], reghits, cloci, assoctval);
-		printf("%s\t%li\t%li\t%s\t%i\t%i\t%li\t%4.4f\n", bed2[j].n, bed2[j].c[0], bed2[j].c[1], bed2[j].f, min, max, assoctval, (float)assoctval/cloci);
-
-		if(istarthere >= m)
-			break;
-	}
 	return;
 }
 
@@ -2810,8 +2653,6 @@ int main(int argc, char *argv[])
 	bgr_t *bgrow=NULL; /* usually macs signal */
 	bgr_t2 *bed2=NULL; /* usually bed file from gff */
 	words_t *bedword=NULL; /* usually feature names of interest */
-	dpf_t *dpf=NULL; /* usually feature names of interest */
-	dpf_t *dpf2=NULL; /* usually feature names of interest */
 	gf_t *gf=NULL; /* usually genome size file */
 	rmf_t *rmf=NULL; /* usually genome size file */
 	gf22_t *gf22=NULL;
@@ -2830,10 +2671,6 @@ int main(int argc, char *argv[])
 		bed2=processinpf2(opts.fstr, &m2, &n2);
 	if(opts.ustr)
 		bedword=processwordf(opts.ustr, &m3, &n3);
-	if(opts.pstr)
-		dpf=processdpf(opts.pstr, &m4, &n4);
-	if(opts.qstr)
-		dpf2=processdpf(opts.pstr, &m4b, &n4b);
 	if(opts.gstr)
 		gf=processgf(opts.gstr, &m5, &n5);
 	if(opts.bstr)
@@ -2920,9 +2757,6 @@ int main(int argc, char *argv[])
 			printf("%s\n", bedword[i].n);
 	}
 
-	if((opts.pstr) && (opts.fstr) )
-		md2bedp(dpf, bed2, m2, m4);
-
 	if((opts.dflg) && (opts.rstr) )
 		prtrmf(opts.rstr, rmf, m6);
 
@@ -2936,8 +2770,11 @@ int main(int argc, char *argv[])
 	}
 
 	if((!opts.dflg) && (opts.ystr) && (opts.ustr) ) {
-		printf("right option chosen.\n");
 		prtgf22n(opts.ystr, gf22, m7, bedword, m3);
+	}
+
+	if((!opts.dflg) && (opts.ystr) && (opts.ustr) && (opts.astr) ) {
+		prtgf22nfa(sqisz, gf22, m7, numsq, bedword, m3);
 	}
 
 	if((opts.dflg) && (opts.hstr) ) {
@@ -2955,9 +2792,6 @@ int main(int argc, char *argv[])
 	if((opts.gstr) && (opts.rstr) )
 		mgf2rmf(opts.gstr, opts.rstr, gf, rmf, m6, m5);
 
-	if((opts.gstr) && (opts.pstr) )
-		mgf2dpf(opts.gstr, opts.pstr, gf, dpf, m4, m5);
-
 	if((opts.gstr) && (opts.fstr) )
 		mgf2bed(opts.gstr, opts.fstr, gf, bed2, m2, m5);
 	// if((opts.ustr) && (opts.fstr) && opts.sflg)
@@ -2970,16 +2804,6 @@ int main(int argc, char *argv[])
 	}
 
 final:
-	if(opts.pstr) {
-		for(i=0;i<m4;++i)
-			free(dpf[i].n);
-		free(dpf);
-	}
-	if(opts.qstr) {
-		for(i=0;i<m4b;++i)
-			free(dpf2[i].n);
-		free(dpf2);
-	}
 	if(opts.istr) {
 		for(i=0;i<m;++i)
 			free(bgrow[i].n);
